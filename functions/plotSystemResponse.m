7e2604d77ff7a04e1e0dab6e5bc77ee9081e0339
Felipe Azzolino Varella <felipe_azzolinovarella@hotmail.com>
2023-12-05 20:07:45 -0300

    Adicionando diagrama de bode em MF e MA

--- functions/plotSystemResponse.m
+++ functions/plotSystemResponse.m
@@ -1,31 +1,49 @@
-function [fig1, fig2, fig3] = plotSystemResponse(open_loop_transfer_function, w_bode, t_ramp, t_step)
-    % closed_loop_transfer_function = feedback(open_loop_transfer_function, 1);
-    
+function [fig1, fig2, fig3, fig4] = plotSystemResponse(open_loop_transfer_function, w_bode, t_ramp, t_step)    
     % Reaproveitando a implementacao da outra funcao
     [peak_time, overshoot, steady_state_error, y_step, y_ramp] = getMetrics(open_loop_transfer_function, t_ramp, t_step);
 
-    % Diagrama de bode
+    % Diagrama de bode em malha aberta
     % [mag, phase] = bode(closed_loop_transfer_function, w_bode);  % Devemos pegar em malha aberta ou fechada?
-    [mag, phase] = bode(open_loop_transfer_function, w_bode);
-    mag = squeeze(mag2db(mag));
-    phase = squeeze(phase);
+    [mag_ol, phase_ol] = bode(open_loop_transfer_function, w_bode);
+    mag_ol = squeeze(mag2db(mag_ol));
+    phase_ol = squeeze(phase_ol);
     [~, phase_margin, ~, wcg] = margin(open_loop_transfer_function);
     fig1 = figure;
     ax11 = subplot(2, 1, 1);
-    semilogx(w_bode, mag)
+    semilogx(w_bode, mag_ol)
     ylabel('Magnitude [dB]')
     xlabel('\omega [rad/s]')
     grid on
     title(sprintf('Diagrama de Bode de malha aberta -  MF = %.2f ° (em %.2f rad/s)', phase_margin, wcg))
     ax12 = subplot(2, 1, 2);
-    semilogx(w_bode, phase)
+    semilogx(w_bode, phase_ol)
+    ylabel('Fase [°]')
+    xlabel('\omega [rad/s]')
+    grid on
+    linkaxes([ax11,ax12],'x')
+
+    % Diagrama de bode em malha fechada
+    closed_loop_transfer_function = feedback(open_loop_transfer_function, 1);
+    [mag_cl, phase_cl] = bode(closed_loop_transfer_function, w_bode);  % Devemos pegar em malha aberta ou fechada?
+    mag_cl = squeeze(mag2db(mag_cl));
+    BW = bandwidth(closed_loop_transfer_function);
+    phase_cl = squeeze(phase_cl);
+    fig2 = figure;
+    ax11 = subplot(2, 1, 1);
+    semilogx(w_bode, mag_cl)
+    ylabel('Magnitude [dB]')
+    xlabel('\omega [rad/s]')
+    grid on
+    title(sprintf('Diagrama de Bode de malha fechada -  BW = %.2f rad/s', BW))
+    ax12 = subplot(2, 1, 2);
+    semilogx(w_bode, phase_cl)
     ylabel('Fase [°]')
     xlabel('\omega [rad/s]')
     grid on
     linkaxes([ax11,ax12],'x')
 
     % Resposta a rampa    
-    fig2 = figure;
+    fig3 = figure;
     plot(t_ramp, y_ramp, 'r')
     hold on
     plot(t_ramp, t_ramp, 'k--')
@@ -36,7 +54,7 @@ function [fig1, fig2, fig3] = plotSystemResponse(open_loop_transfer_function, w_
     grid on
     
     % Reposta ao degrau
-    fig3 = figure;
+    fig4 = figure;
     plot(t_step, y_step, 'r')
     hold on
     plot([t_step(1) t_step(end)], [1, 1], 'k--')  % Referencia: r(t) = 1
--- main.m
+++ main.m
@@ -16,15 +16,16 @@ set(0, 'DefaultLineLineWidth', 2, ...
 P = buildPlant();
 
 % Validacao do nao compensado sistema
-w_bode = logspace(-1, 4, 1E3);
+w_bode = logspace(-2, 4, 1E3);
 t_ramp = linspace(0, 50, 1E3);
 t_step = linspace(0, 50, 1E3);
-[fig1, fig2, fig3] =  plotSystemResponse(P, w_bode, t_ramp, t_step);
+[fig1, fig2, fig3, fig4] =  plotSystemResponse(P, w_bode, t_ramp, t_step);
 
 % Exportando figuras
-saveFig(fig1, './figs/bode_nocomp.png')
-saveFig(fig2, './figs/ramp_nocomp.png')
-saveFig(fig3, './figs/step_nocomp.png')
+saveFig(fig1, './figs/bode_ol_nocomp.png')
+saveFig(fig2, './figs/bode_cl_nocomp.png')
+saveFig(fig3, './figs/ramp_nocomp.png')
+saveFig(fig4, './figs/step_nocomp.png')
 
 %% Compensador por avanco
 
@@ -36,7 +37,7 @@ mp_lead = 5/100;
 MF_lead_start = 45; % Valor inicial
 
 % Para encontrarmos o melhor compensador de forma automatica
-w_bode_lead = logspace(-1, 4, 1E3);  % Tempo muda de um compensador pro outro! 
+w_bode_lead = logspace(-2, 4, 1E3);  % Tempo muda de um compensador pro outro! 
 t_ramp_lead = linspace(0, 5, 1E3);
 t_step_lead = linspace(0, 1, 1E3);
 
@@ -51,12 +52,13 @@ for MF_lead = MF_lead_start:5:180
 end
 
 % Validacao do sistema compensado
-[fig1_lead, fig2_lead, fig3_lead] = plotSystemResponse(Gc_lead*P, w_bode_lead, t_ramp_lead, t_step_lead);
+[fig1_lead, fig2_lead, fig3_lead, fig4_lead] = plotSystemResponse(Gc_lead*P, w_bode_lead, t_ramp_lead, t_step_lead);
 
 % Exportando figuras
-saveFig(fig1_lead, './figs/bode_lead.png')
-saveFig(fig2_lead, './figs/ramp_lead.png')
-saveFig(fig3_lead, './figs/step_lead.png')
+saveFig(fig1_lead, './figs/bode_ol_lead.png')
+saveFig(fig2_lead, './figs/bode_cl_lead.png')
+saveFig(fig3_lead, './figs/ramp_lead.png')
+saveFig(fig4_lead, './figs/step_lead.png')
 
 %% Compensador por atraso
 
@@ -69,7 +71,7 @@ mp_lag = 10/100;  % Aumenta um pouco mais o pss mas diminui o ts (melhor trade-o
 MF_lag_start = 45;  % Valor inicial
 
 % Para encontrarmos o melhor compensador de forma automatica
-w_bode_lag = logspace(-1, 4, 1E3);  % Tempo muda de um compensador pro outro!
+w_bode_lag = logspace(-2, 4, 1E3);  % Tempo muda de um compensador pro outro!
 % t_ramp_lag = linspace(0, 1E4, 1E3);  % Para mp_lag = 5/100 (mais lento)
 % t_step_lag = linspace(0, 400, 1E4);
 t_ramp_lag = linspace(0, 1E2, 1E3);  % Para mp_lag = 10/100 (mais rapido)
@@ -86,12 +88,13 @@ for MF_lag = MF_lag_start:5:180
 end
 
 % Validacao do sistema compensado
-[fig1_lag, fig2_lag, fig3_lag] = plotSystemResponse(Gc_lag*P, w_bode_lag, t_ramp_lag, t_step_lag);
+[fig1_lag, fig2_lag, fig3_lag, fig4_lag] = plotSystemResponse(Gc_lag*P, w_bode_lag, t_ramp_lag, t_step_lag);
 
 % Exportando figuras
-saveFig(fig1_lag, './figs/bode_lag.png')
-saveFig(fig2_lag, './figs/ramp_lag.png')
-saveFig(fig3_lag, './figs/step_lag.png')
+saveFig(fig1_lag, './figs/bode_ol_lag.png')
+saveFig(fig2_lag, './figs/bode_cl_lag.png')
+saveFig(fig3_lag, './figs/ramp_lag.png')
+saveFig(fig4_lag, './figs/step_lag.png')
 
 %% Compensador por avanco-atraso
 
@@ -104,7 +107,7 @@ tp_leadlag = 0.1;  % Aumenta um pouco mais o pss mas diminui o ts (melhor trade-
 tol_leadlag_start = 0;  % Valor inicial
 
 % Para encontrarmos o melhor compensador de forma automatica
-w_bode_leadlag = logspace(-1, 4, 1E3);  % Tempo muda de um compensador pro outro!
+w_bode_leadlag = logspace(-2, 4, 1E3);  % Tempo muda de um compensador pro outro!
 t_ramp_leadlag = linspace(0, 10, 1E3);
 t_step_leadlag = linspace(0, 1, 1E3);
 
@@ -119,9 +122,10 @@ for tol_leadlag = tol_leadlag_start:1:20
 end
 
 % Validacao do sistema compensado
-[fig1_leadlag, fig2_leadlag, fig3_leadlag] = plotSystemResponse(Gc_leadlag*P, w_bode_leadlag, t_ramp_leadlag, t_step_leadlag);
+[fig1_leadlag, fig2_leadlag, fig3_leadlag, fig4_leadlag] = plotSystemResponse(Gc_leadlag*P, w_bode_leadlag, t_ramp_leadlag, t_step_leadlag);
 
 % Exportando figuras
-saveFig(fig1_leadlag, './figs/bode_leadlag.png')
-saveFig(fig2_leadlag, './figs/ramp_leadlag.png')
-saveFig(fig3_leadlag, './figs/step_leadlag.png')
+saveFig(fig1_leadlag, './figs/bode_ol_leadlag.png')
+saveFig(fig2_leadlag, './figs/bode_cl_leadlag.png')
+saveFig(fig3_leadlag, './figs/ramp_leadlag.png')
+saveFig(fig4_leadlag, './figs/step_leadlag.png')
